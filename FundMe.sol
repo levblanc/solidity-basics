8439b3fd8385ed35459b09360795d92feea3b393
levblanc <levblanc@gmail.com>
2022-09-03 11:27:08 +0800

    Add `for` loop and `call` examples

--- FundMe.sol
+++ FundMe.sol
@@ -23,6 +23,23 @@ contract FundMe {
         addressToAmountFunded[msg.sender] = msg.value;
     }
 
-    
+    function withdraw() public {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0; 
+        }
+
+        // reset the array
+        funders = new address[](0);
+
+        // actually withdraw the funds
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}('');
+        require(callSuccess, 'Call failed');
+    }
 }
 
